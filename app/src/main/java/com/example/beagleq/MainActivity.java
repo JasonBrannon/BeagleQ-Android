a4e0dd49fd0aeec7182c5dbcc7fbf2792e6af27b
Jason Brannon <campbellgolf@gmail.com>
2022-11-15 17:00:30 -0500

    PID and Socket Connection

--- app/src/main/java/com/example/beagleq/MainActivity.java
+++ app/src/main/java/com/example/beagleq/MainActivity.java
@@ -6,16 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
-import java.security.spec.ECField;
 
 
 import okhttp3.OkHttpClient;
@@ -27,12 +24,17 @@ import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
 
-    Thread Thread1 = null;
-    private Button start;
-    private TextView output;
+    private Thread Thread1 = null;
+    private Button btnStart, btnConnect;
+    private TextView output, tvPitTemp, tvFanInfo;
     private OkHttpClient client;
-    private TextView tvMessages;
-    EditText etIP, etPort;
+    private TextView tvConnStatus;
+    private EditText etIP, etPort;
+    private Switch switchFan;
+
+    //Web Socket
+    private WebSocket ws;
+
 
     String SERVER_IP;
     int SERVER_PORT;
@@ -49,18 +51,13 @@ public class MainActivity extends AppCompatActivity {
                 webSocket.send(json.toString());
 
             } catch (Exception ex){
-                tvMessages.setText(ex.toString());
+                tvConnStatus.setText(ex.toString());
             }
-
-            //webSocket.send("Hello, it's SSaurel !");
-            //webSocket.send("What's up ?");
-           // webSocket.send(ByteString.decodeHex("deadbeef"));
-            //webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye !");
         }
 
         @Override
-        public void onMessage(WebSocket webSocket, String text) {
-            output("Receiving : " + text);
+        public void onMessage(WebSocket webSocket, String receivedPayload) {
+            output(receivedPayload);
         }
 
         @Override
@@ -84,11 +81,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        start = (Button) findViewById(R.id.btnSend);
-        output = (TextView) findViewById(R.id.tvMessages);
+        btnStart = (Button) findViewById(R.id.btnSend);
+        btnConnect = findViewById(R.id.btnConnect);
+        tvConnStatus = findViewById(R.id.tvConnStatus);
+        tvPitTemp = findViewById(R.id.tvPitTemp);
+        tvFanInfo = findViewById(R.id.tvFanInfo);
+        switchFan = findViewById(R.id.switchFan);
+        output = (TextView) findViewById(R.id.tvConnStatus);
+        etIP = findViewById(R.id.etIP);
+        etPort = findViewById(R.id.etPort);
         client = new OkHttpClient();
-
-
 
         Button btnConnect = findViewById(R.id.btnConnect);
         btnConnect.setOnClickListener(new View.OnClickListener() {
@@ -96,47 +98,157 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 try {
-                    tvMessages.setText("");
+                    tvConnStatus.setText("");
                     SERVER_IP = etIP.getText().toString().trim();
                     SERVER_PORT = Integer.parseInt(etPort.getText().toString().trim());
 
                     if (SERVER_IP.length() == 0 || SERVER_PORT == 0) {
-                        tvMessages.setText("Must set the Server IP and Port");
+                        tvConnStatus.setText("Must set the Server IP and Port");
                     } else {
                         Thread1 = new Thread(new Thread1());
                         Thread1.start();
                     }
                 } catch (Exception ex){
-                    tvMessages.setText("Must set the Server IP and Port");
+                    tvConnStatus.setText("Must set the Server IP and Port");
                 }
             }
         });
 
-        start.setOnClickListener(new View.OnClickListener() {
+        switchFan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                start();
+
+                JSONObject json = new JSONObject();
+
+                try {
+                    json.put("topic", "pidToggle");
+                    if (switchFan.isChecked()) {
+                        json.put("data", "1");
+                    } else {
+                        json.put("data", "0");
+                    }
+                    ws.send(json.toString());
+                } catch (Exception e) {
+                    tvConnStatus.setText("Fan error, you're not connected!");
+                }
+
+            }
+        });
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connect();
             }
         });
 
     }
 
-    private void start() {
+    private void connect() {
         Request request = new Request.Builder().url("ws://10.69.8.179:8086").build();
         EchoWebSocketListener listener = new EchoWebSocketListener();
         WebSocket ws = client.newWebSocket(request, listener);
-
         client.dispatcher().executorService().shutdown();
     }
 
-    private void output(final String txt) {
+    private void updateTemps(JSONObject message) throws JSONException {
+        String PIT = String.format("%s F", message.getString("pitTemp"));
+        tvPitTemp.setText(PIT);
+    }
+
+    private void updateFanStatus(JSONObject message) throws JSONException {
+        if (message.getString("state").equals("1")){
+            tvFanInfo.setText("Running");
+        } else {
+            tvFanInfo.setText("Off");
+        }
+    }
+
+    private void output(final String receivedPayload) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                output.setText(output.getText().toString() + "\n\n" + txt);
+                // Parse server message payload for data points
+                //output.setText(output.getText().toString() + "\n\n" + receivedPayload);
+
+                try {
+                    JSONObject message = new JSONObject(receivedPayload);
+                    String topic = message.getString("topic");
+                    switch (topic){
+                        case "info":
+                            //updateInfo(message);
+                            break;
+                        case "temperatures":
+                            updateTemps(message);
+                            break;
+                        case "beagleInfo":
+                            //updateBeagleInfo(message);
+                            break;
+                        case "fanStatus":
+                            updateFanStatus(message);
+                            break;
+                        case "pitAlert":
+                            //updatePitAlert(message);
+                            break;
+                        default:
+                            //document.getElementById('iInfo').innerHTML = "Unknown incoming message" + incoming.topic;
+
+                    }
+                } catch (Exception ex){
+                     tvConnStatus.setText("Fatal Blow! Server Down");
+                }
+
             }
         });
     }
+
+    class Thread1 implements Runnable {
+        public void run() {
+            Socket socket;
+            try {
+                Request request = new Request.Builder().url("ws://" + SERVER_IP + ":" + SERVER_PORT + "").build();
+                EchoWebSocketListener listener = new EchoWebSocketListener();
+                ws = client.newWebSocket(request, listener);
+                client.dispatcher().executorService().shutdown();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tvConnStatus.setText("Connected");
+                    }
+                });
+                //new Thread(new Thread2()).start();
+            } catch (Exception e) {
+                tvConnStatus.setText("Unable to connect. Verify IP and Port");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /*
+    class Thread2 implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    final String message = input.readLine();
+                    if (message != null) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tvMessages.append("server: " + message + " ");
+                            }
+                        });
+                    } else {
+                        Thread1 = new Thread(new Thread1());
+                        Thread1.start();
+                        return;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }*/
 
 
 
